4f4e79cefebe911a8d124bab43f3b53ddfb09648
Andrew Smith <andrew@andrew.codes>
2016-10-27 18:08:49 -0400

    Share eslint configs with Vue and Core/other products

---
+++ loose.js
@@ -0,0 +1,11 @@
+module.exports = {
+    globals: {
+        suite: true,
+        test: true,
+        expect: true,
+        should: true
+    },
+    rules: {
+        semi: ["warn", 'always']
+    }
+};
---
+++ strict.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    globals: {
+        suite: true,
+        test: true,
+        expect: true,
+        should: true
+    },
+    rules: {
+        semi: ["error", 'always']
+    }
+
+};
